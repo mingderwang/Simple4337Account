70da52ddc34727c3c1d2a4ff14d86eb67b85b0cf
mingderwang <mingderwang@me.com>
2024-09-22 14:31:12 -0700

    add Infura test on index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import { getKernelClient } from './utils'
+import  axios from 'axios';
+
+const main = async () => {
+  const infuraUrl = `https://linea-mainnet.infura.io/v3/${process.env.INFURA_ID}`;
+  try {
+  const response = await axios.post(infuraUrl, {
+            jsonrpc: '2.0',
+            id: 1,
+            method: 'eth_blockNumber',
+            params: []
+        });
+
+  const latestBlock = response.data.result;
+  console.log(`linea-mainnet block hight: ${latestBlock}`);
+  } catch (error) {
+        console.error('Error fetching latest block:', error);
+    }
+}
+
+main()
